5c82a7dd993efee153784084e31e4127f9ca07e9
isaacmayela <135630765+isaacmayela@users.noreply.github.com>
2023-08-13 18:30:50 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+// Do not remove this function definition and its export
+// this function should return the result of the current operation
+function calculate(expression) {
+    return expression;
+  }
+  
+  export { calculate };
+  
